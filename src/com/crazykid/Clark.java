0fe83867ea8870859fba18c97bbc03ed748d0dd0
liujianfeng <543140360@qq.com>
2016-01-29 22:32:08 +0800

    完成kata初级练习2

--- src/com/crazykid/Clark.java
+++ src/com/crazykid/Clark.java
@@ -4,8 +4,16 @@ package com.crazykid;
  * Created by ljf on 16-1-29.
  */
 public class Clark {
+    private String hello = "Hello";
 
     public String hello() {
-        return "Hello";
+        return hello;
+    }
+
+    public String hello(String name) {
+        if (null == name) {
+            throw new NullPointerException("The name cannot be null");
+        }
+        return hello + ", " + name;
     }
 }
--- test/com/crazykid/TestSayHelloWorld.java
+++ test/com/crazykid/TestSayHelloWorld.java
@@ -1,6 +1,11 @@
 package com.crazykid;
 
+import junit.framework.Assert;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static junit.framework.TestCase.assertEquals;
 
@@ -8,12 +13,44 @@ import static junit.framework.TestCase.assertEquals;
  * Created by ljf on 16-1-29.
  */
 public class TestSayHelloWorld {
+    private Clark clark = null;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
     private String originHello = "Hello";
+    private String bob = "Bob";
+    private String alice = "Alice";
+    private String tom = "Tom";
+    private String nullName = null;
+
+    @Before
+    public void setup() {
+        clark = new Clark();
+    }
 
     @Test
     public void testSayHelloWorld() {
-        Clark clark = new Clark();
         String hello = clark.hello();
         assertEquals(originHello, hello);
+    }
+
+    @Test
+    public void testSayHelloToSomeone() {
+        assertEquals("Hello, Bob", clark.hello(bob));
+        assertEquals("Hello, Alice", clark.hello(alice));
+        assertEquals("Hello, Tom", clark.hello(tom));
+    }
+
+    @Test
+    public void testSayHelloToNull() {
+        expectedException.expect(NullPointerException.class);
+        expectedException.expectMessage("The name cannot be null");
+        clark.hello(null);
+    }
+
+    @After
+    public void tearDown() {
+        clark = null;
     }
 }
