bc801eda41aa1e0f91872526442831aeb700ce81
liujianfeng <543140360@qq.com>
2016-01-29 21:53:01 +0800

    完成kata初级的第7个练习：写程序打印出12 * 12的乘法表

--- src/com/crazykid/MultiplicationTable.java
+++ src/com/crazykid/MultiplicationTable.java
@@ -1,7 +1,51 @@
 package com.crazykid;
 
 public class MultiplicationTable {
-    public String getTable() {
-        return "0";
+    public String getTable(int factor) {
+        if (factor > 0) {
+            return getTableWhenFactorIsPosition(factor);
+        } else {
+            throw new IllegalArgumentException("The factor must be positive.");
+        }
+    }
+
+    private String getTableWhenFactorIsPosition(int factor) {
+        StringBuilder multiplicationTable = new StringBuilder();
+        for (int i = 1; i <= factor; i++) {
+            buildLine(multiplicationTable, i);
+            deleteSpaceAndCommaOfTheEndOfLine(multiplicationTable);
+            appendTheEnter(multiplicationTable);
+        }
+        deleteTheLastEnter(multiplicationTable);
+        return multiplicationTable.toString();
+    }
+
+    private void buildLine(StringBuilder multiplicationTable, int i) {
+        for (int j = 1; j <= i; j++) {
+            multiplicationTable
+                    .append(i)
+                    .append("*")
+                    .append(j)
+                    .append(" = ")
+                    .append(i * j)
+                    .append(", ");
+        }
+    }
+
+    private void deleteSpaceAndCommaOfTheEndOfLine(StringBuilder multiplicationTable) {
+        deleteTheLastComma(multiplicationTable);
+        deleteTheLastEnter(multiplicationTable);
+    }
+
+    private void deleteTheLastComma(StringBuilder multiplicationTable) {
+        multiplicationTable.deleteCharAt(multiplicationTable.length() - 2);
+    }
+
+    private void deleteTheLastEnter(StringBuilder multiplicationTable) {
+        multiplicationTable.deleteCharAt(multiplicationTable.length() - 1);
+    }
+
+    private void appendTheEnter(StringBuilder multiplicationTable) {
+        multiplicationTable.append("\n");
     }
 }
--- test/com/crazykid/TestMultiplicationTable.java
+++ test/com/crazykid/TestMultiplicationTable.java
@@ -1,22 +1,51 @@
 package com.crazykid;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static junit.framework.TestCase.assertEquals;
 public class TestMultiplicationTable {
     MultiplicationTable multiplicationTable = null;
-    String table = "1*1 = 1\n" +
-                   "2*1 = 2, 2*2 = 4\n" +
-                   "3*1 = 3, 3*2 = 6, 3*3 = 9\n" +
-                   "4*1 = 4, 4*2 = 8, 4*3 = 12, 4*4 = 16";
+    @Rule public ExpectedException expectedException = ExpectedException.none();
+
+    String tableFourTimes = "1*1 = 1\n" +
+                            "2*1 = 2, 2*2 = 4\n" +
+                            "3*1 = 3, 3*2 = 6, 3*3 = 9\n" +
+                            "4*1 = 4, 4*2 = 8, 4*3 = 12, 4*4 = 16";
+    String tableOneTime = "1*1 = 1";
+    String tableSixTimes = "1*1 = 1\n" +
+                           "2*1 = 2, 2*2 = 4\n" +
+                           "3*1 = 3, 3*2 = 6, 3*3 = 9\n" +
+                           "4*1 = 4, 4*2 = 8, 4*3 = 12, 4*4 = 16\n" +
+                           "5*1 = 5, 5*2 = 10, 5*3 = 15, 5*4 = 20, 5*5 = 25\n" +
+                           "6*1 = 6, 6*2 = 12, 6*3 = 18, 6*4 = 24, 6*5 = 30, 6*6 = 36";
     @Before
     public void setUp() {
         multiplicationTable = new MultiplicationTable();
     }
 
     @Test
-    public void testPrintMultiplicationTable() {
-        assertEquals(table, multiplicationTable.getTable());
+    public void testFourTimesTable() {
+        assertEquals(tableFourTimes, multiplicationTable.getTable(4));
+    }
+
+    @Test
+    public void testOneTimeTable() {
+        assertEquals(tableOneTime, multiplicationTable.getTable(1));
+    }
+
+    @Test
+    public void testSixTimesTable() {
+        assertEquals(tableSixTimes, multiplicationTable.getTable(6));
+    }
+
+    @Test
+    public void testZeroOrNegativeTable() {
+        expectedException.expect(IllegalArgumentException.class);
+        expectedException.expectMessage("The factor must be positive.");
+        multiplicationTable.getTable(0);
+        multiplicationTable.getTable(-1);
     }
 }
